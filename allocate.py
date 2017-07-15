c3a5fbf9b6c18f801ec41300cc501ed2ed748e89
zaiquiri <zdl2102@columbia.edu>
2017-07-15 15:37:22 -0400

    ok

--- allocate.py
+++ allocate.py
@@ -1,6 +1,14 @@
 import math
 from random import shuffle
 
+### KNOBS ###
+
+# How many shares to "back-off" from the naive starting allocation
+STARTING_BACKOFF = 2;
+# How much over the golden allocation is too much to warrent continuing
+OVER_ALLOCATION_THRESHOLD = 1.05
+
+# GLOBALS
 PRICES = {"VTI":126.23, "VEA":42.07, "VWO":42.52, "VIG":93.49, "VNQ":83.08, "VCIT":87.7, "VWOB":80.04}
 TARGET_PERCENTAGES = {"VTI":0.21, "VEA":0.18, "VWO":0.22, "VIG":0.13, "VNQ":0.16, "VCIT":0.05, "VWOB":0.05, "cash":0}
 TOTAL_CASH = 19205.71
@@ -12,8 +20,7 @@ def main():
     allocations = get_starting_allocations(PERFECT_AMOUNTS)
     BEST_SO_FAR["best"] = allocations.copy()
     allocate(allocations)
-    avg_delta = get_avg_delta(BEST_SO_FAR["best"])
-    print "OPTIMAL DELTA: " + str(avg_delta) + " ::: " + str(BEST_SO_FAR["best"])
+    print "OPTIMAL ALLOCATION: " + str(BEST_SO_FAR["best"])
 
 def allocate(allocations):
     purchase_options = get_possisible_purchases(allocations["cash"])
@@ -21,7 +28,7 @@ def allocate(allocations):
         current_avg_delta = get_avg_delta(allocations)
         best_so_far_delta = get_avg_delta(BEST_SO_FAR["best"])
         if (current_avg_delta < best_so_far_delta):
-            print "BEST DELTA: " + str(current_avg_delta) + " ALLOCATIONS: " + str(allocations)
+            print "Best deviation so far " + str(current_avg_delta) + " ..."
             BEST_SO_FAR["best"] = allocations.copy()
     else:
         for purchase in purchase_options:
@@ -65,15 +72,14 @@ def seen_combination(allocations):
         return False;
 
 def gratutitous_allocation(purchase, allocations):
-    return allocations[purchase] * PRICES[purchase] / PERFECT_AMOUNTS[purchase] > 1.05
+    return allocations[purchase] * PRICES[purchase] / PERFECT_AMOUNTS[purchase] > OVER_ALLOCATION_THRESHOLD
 
 def get_starting_allocations(perfect_amounts):
     allocations = {}
-    optimization_buffer = 2
     cash = TOTAL_CASH;
     for asset, amount in perfect_amounts.iteritems():
         if not asset == "cash":
-            allocations[asset] = math.floor(perfect_amounts[asset] / PRICES[asset]) - optimization_buffer
+            allocations[asset] = math.floor(perfect_amounts[asset] / PRICES[asset]) - STARTING_BACKOFF
             cash -= allocations[asset] * PRICES[asset]
     allocations["cash"] = cash
     return allocations
